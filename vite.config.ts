d2e11a7857cdb6162df4e04b7485b5fa4c1071fe
Benjamin VIELLARD <gbicou@gmail.com>
2023-11-22 17:53:35 +0100

    chore: vite builds difference

--- vite.config.ts
+++ vite.config.ts
@@ -6,43 +6,48 @@ import nodeExternals from "rollup-plugin-node-externals";
 import VueRouter from "unplugin-vue-router/vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     VueRouter(),
     vue(),
-    dts({
-      include: ["src"],
-      exclude: ["src/main.ts", "src/App.vue", "src/router.ts"],
-      cleanVueFileName: true,
-      insertTypesEntry: true,
-    }),
-    {
-      ...nodeExternals(),
-      enforce: "pre",
-    },
-  ],
-  build: {
-    target: "esnext",
-    copyPublicDir: false,
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "VueProsemirrorRender",
-      // the proper extensions will be added
-      fileName: "vue-prosemirror-render",
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      // plugins: [nodeExternals({})],
-      // external: ['vue', 'change-case'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: "Vue",
-        },
+    mode === "lib" && [
+      dts({
+        include: ["src"],
+        exclude: ["src/main.ts", "src/App.vue", "src/router.ts"],
+        cleanVueFileName: true,
+        insertTypesEntry: true,
+      }),
+      {
+        ...nodeExternals(),
+        enforce: "pre",
       },
-    },
-  },
-});
+    ],
+  ],
+  build:
+    mode === "lib"
+      ? {
+          target: "esnext",
+          copyPublicDir: false,
+          lib: {
+            // Could also be a dictionary or array of multiple entry points
+            entry: resolve(__dirname, "src/index.ts"),
+            name: "VueProsemirrorRender",
+            // the proper extensions will be added
+            fileName: "vue-prosemirror-render",
+          },
+          rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            // plugins: [nodeExternals({})],
+            // external: ['vue', 'change-case'],
+            output: {
+              // Provide global variables to use in the UMD build
+              // for externalized deps
+              globals: {
+                vue: "Vue",
+              },
+            },
+          },
+        }
+      : undefined,
+}));
