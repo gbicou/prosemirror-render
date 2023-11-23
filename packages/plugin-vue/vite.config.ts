4d2183d5f623468d3f0e789718b3ae12cc051547
Benjamin VIELLARD <bicou@bicou.com>
2023-11-23 18:53:14 +0100

    test: create vitest env

--- packages/plugin-vue/vite.config.ts
+++ packages/plugin-vue/vite.config.ts
@@ -1,8 +1,10 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import dts from "vite-plugin-dts";
 import { resolve } from "path";
 import nodeExternals from "rollup-plugin-node-externals";
+import GithubActionsReporter from "vitest-github-actions-reporter";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,5 +27,10 @@ export default defineConfig({
       fileName: "plugin",
     },
     rollupOptions: {},
+  },
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    reporters: process.env.GITHUB_ACTIONS ? ["default", new GithubActionsReporter()] : "default",
   },
 });
