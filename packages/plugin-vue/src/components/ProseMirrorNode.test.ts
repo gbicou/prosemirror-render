13d17f38ab663340fe064742115c7d00edbfa49b
Benjamin VIELLARD <bicou@bicou.com>
2023-11-23 18:57:35 +0100

    test: clean

--- packages/plugin-vue/src/components/ProseMirrorNode.test.ts
+++ packages/plugin-vue/src/components/ProseMirrorNode.test.ts
@@ -25,8 +25,6 @@ describe("ProseMirrorNode component", () => {
       },
     });
 
-    expect(wrapper.text()).toContain("fail");
-
     expect(wrapper.text()).toContain("content");
     expect(wrapper.html()).toMatchSnapshot();
   });
