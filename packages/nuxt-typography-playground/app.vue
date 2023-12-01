f440e0406ccd775eba46151df8222418b3c06730
Benjamin VIELLARD <bicou@bicou.com>
2023-12-01 23:51:40 +0100

    docs(nuxt-playground): add book example (long document)

--- packages/nuxt-typography-playground/app.vue
+++ packages/nuxt-typography-playground/app.vue
@@ -1,13 +1,37 @@
-<script setup lang="ts">
-import basic from "#examples/basic.json";
-</script>
-
 <template>
-  <prosemirror-render id="basic" :node="basic" />
+  <nav>
+    <h1>Nuxt ProseMirror typography playground</h1>
+    <ul>
+      <li><nuxt-link to="/">index</nuxt-link></li>
+      <li><nuxt-link to="/book">book (~6000 nodes)</nuxt-link></li>
+    </ul>
+  </nav>
+  <main>
+    <nuxt-page />
+  </main>
 </template>
 
-<style>
-#basic {
+<style scoped>
+nav {
+  background: #bfd5c7;
+  color: #2b332e;
+  padding: 2em;
+
+  h1 {
+    font-size: 2em;
+  }
+
+  ul {
+    display: flex;
+    li {
+      margin-right: 1em;
+      a {
+        text-decoration: underline;
+      }
+    }
+  }
+}
+main {
   margin: 0 auto;
   max-width: 80ch;
   padding: 2em;
---
+++ packages/nuxt-typography-playground/pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import basic from "#examples/basic.json";
+</script>
+
+<template>
+  <prosemirror-render :node="basic" />
+</template>
