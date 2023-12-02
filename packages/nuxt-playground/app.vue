853b584116b8b6f32da28d12215adf205c14f5a5
Benjamin VIELLARD <bicou@bicou.com>
2023-12-02 13:24:16 +0100

    docs(nuxt-playground): add links to playground

--- packages/nuxt-playground/app.vue
+++ packages/nuxt-playground/app.vue
@@ -1,9 +1,53 @@
 <template>
-  <div>
-    <h1>Nuxt module playground!</h1>
+  <header>
+    <h1>Nuxt ProseMirror playground</h1>
+    <ul>
+      <li>
+        <nuxt-link to="/"> index </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/table"> table </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/book"> book (~6000 nodes) </nuxt-link>
+      </li>
+    </ul>
+  </header>
+  <main>
     <nuxt-page />
-  </div>
+  </main>
 </template>
 
-<script setup lang="ts">
-</script>
+<style scoped lang="postcss">
+header {
+  background: #bfd5c7;
+  color: #2b332e;
+  padding: 2em;
+
+  & a {
+    color: inherit;
+    text-decoration: underline;
+  }
+
+  & h1 {
+    margin: 0;
+    font-size: 2em;
+  }
+
+  & ul {
+    margin: 1em 0 0 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+
+    & li {
+      margin: 0 1em 0 0;
+    }
+  }
+}
+main {
+  margin: 0 auto;
+  max-width: 80ch;
+  padding: 2em;
+}
+</style>
--- packages/nuxt-playground/nuxt.config.ts
+++ packages/nuxt-playground/nuxt.config.ts
@@ -2,4 +2,9 @@ export default defineNuxtConfig({
   extends: "../nuxt-module",
   modules: ["../nuxt-module/src/module"],
   devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      "postcss-nesting": {},
+    },
+  },
 });
