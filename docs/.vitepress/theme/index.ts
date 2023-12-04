7f00ccc70b8f7db9291499a5d05a18ed1d51a7c3
Benjamin VIELLARD <bicou@bicou.com>
2023-12-04 23:25:31 +0100

    docs: custom css

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import ProsemirrorRenderVue from "@bicou/prosemirror-render-vue";
+import './custom.css'
 
 export default {
   extends: DefaultTheme,
