36362624db02e8f99d270c26056b1964904dfaaa
Benjamin VIELLARD <bicou@bicou.com>
2024-06-26 10:34:45 +0200

    fix(vue-playground): typed router configuration

--- packages/vue-playground/src/router.ts
+++ packages/vue-playground/src/router.ts
@@ -1,5 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router/auto";
+import { createRouter, createWebHistory } from "vue-router";
+import { routes } from "vue-router/auto-routes";
 
 export default createRouter({
   history: createWebHistory(),
+  // pass the generated routes written by the plugin 🤖
+  routes,
 });
