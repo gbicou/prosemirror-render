f154a7f3e5992df5aa591e6c5591edc0f4698e39
Benjamin VIELLARD <gbicou@gmail.com>
2025-01-27 10:15:23 +0100

    fix lint

--- packages/vue-playground/vite.config.ts
+++ packages/vue-playground/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueRouter from 'unplugin-vue-router/vite'
 import path from 'node:path'
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
